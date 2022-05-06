1c77bba7add957bf3b97a130eeb6822ffd4fab5d
linan@corp_laptop_vm_node1 <believelinan@aliyun.com>
2022-05-07 03:15:35 +0800

    enhancement: make logger.py MyFormatter timezone aware

--- app/lib/mylogger.py
+++ app/lib/mylogger.py
@@ -4,6 +4,24 @@ import os
 
 from app.myglobals import logfolder
 
+####
+import datetime
+import tzlocal
+from dateutil import tz
+
+class MyFormatter(logging.Formatter):
+    """override logging.Formatter to use an aware datetime object"""
+    localzone_name = tzlocal.get_localzone_name()
+    def converter(self, timestamp):
+        dt = datetime.datetime.fromtimestamp(timestamp)
+        # 1.aws gecloud1 timezone is "UTC"
+        # 2.china timezone is "Asia/Shanghai"
+        return dt.replace(tzinfo=tz.gettz(MyFormatter.localzone_name)).astimezone(tz=tz.gettz('Asia/Shagnhai'))
+
+    def formatTime(self, record, datefmt="%Y-%m-%d %H:%M:%S.%f"):
+        dt = self.converter(record.created)
+        return dt.strftime("%Y-%m-%d %H:%M:%S.%f")
+####
 
 logfile = os.path.abspath(os.path.join(logfolder, "log.txt"))
 
@@ -13,7 +31,8 @@ logger = logging.getLogger(__name__)
 
 # logger config
 logger.setLevel(level = logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+# formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = MyFormatter('%(asctime)s - %(levelname)s - %(message)s')
 
 handler = RotatingFileHandler(logfile, maxBytes = 1*1024*100, backupCount=3)
 handler.setLevel(logging.INFO)
