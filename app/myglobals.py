b74057270c8a11a06b3008e5a10a8c89d7378602
linan@corp_laptop_vm_node1 <believelinan@aliyun.com>
2022-05-19 15:47:10 +0800

    blue_main.py: add favicon.ico route

--- app/myglobals.py
+++ app/myglobals.py
@@ -3,3 +3,4 @@ import os
 topdir = os.path.abspath(os.path.join(os.path.dirname(__file__),".."))
 uploadfolder = os.path.abspath(os.path.join(topdir, "upload"))
 logfolder = os.path.abspath(os.path.join(topdir, "log"))
+staticfolder = os.path.abspath(os.path.join(topdir, 'app', 'static'))
--- app/views/blue_main.py
+++ app/views/blue_main.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, send_from_directory
+import os
+
+from app.myglobals import staticfolder
 
 blue_main = Blueprint('blue_main', __name__)
 
@@ -6,3 +9,8 @@ blue_main = Blueprint('blue_main', __name__)
 @blue_main.route('/index/')
 def index():
     return render_template('main_index.html')
+
+@blue_main.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(staticfolder, 'pic'),
+                               'cbyge.png', mimetype='image/vnd.microsoft.icon')
